c7abac931c389a4f3d02eae7d62c19557ac7f5ff
seminchoi <choicco89@gmail.com>
2023-09-15 18:52:49 +0900

    fix : cors 해결

--- src/main/java/smu/poodle/smnavi/common/config/WebMvcConfig.java
+++ src/main/java/smu/poodle/smnavi/common/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://smnavi.me")
+                .allowedOrigins("http://localhost:3000", "https://www.smnavi.me")
                 .allowedHeaders("*")
                 .exposedHeaders("*")
                 .allowCredentials(true)
