664d2c36cb6e2d8b56f4703dae810102c33430b6
seminchoi <choicco89@gmail.com>
2024-03-30 17:16:34 +0900

    refactor : 필요 없는 의존성 제거

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,10 @@ plugins {
     java
     id("org.springframework.boot") version "3.0.1"
     id("io.spring.dependency-management") version "1.1.0"
-//	id ("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
 }
 
 group = "smu.poodle"
-version = "0.0.1-SNAPSHOT"
+version = "2.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 configurations {
@@ -24,11 +23,11 @@ tasks.getByName<Jar>("jar") {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("org.springframework.boot:spring-boot-starter-batch")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-batch")
 
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -37,12 +36,8 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap:4.0.1")
     implementation("org.springframework.cloud:spring-cloud-starter-aws-secrets-manager-config:2.2.6.RELEASE")
 
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-
     runtimeOnly("com.mysql:mysql-connector-j")
 
-    implementation("junit:junit:4.13.2")
     implementation("org.json:json:20230227")
 
     compileOnly("org.projectlombok:lombok")
@@ -52,8 +47,8 @@ dependencies {
     runtimeOnly ("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly ("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
-//	implementation ("com.querydsl:querydsl-jpa")
-//	implementation ("com.querydsl:querydsl-apt")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
